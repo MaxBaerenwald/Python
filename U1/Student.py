5e14332684782af2c6f1de0cf5f58aa49a0717f6
Max Baerenwald <maxbaerenwald@t-online.de>
2020-03-23 21:38:40 +0100

    test

--- U1/Student.py
+++ U1/Student.py
@@ -1,9 +1,26 @@
 class Student:
         """ documentation"""
+        course = ["Mathe", "Prog", "OOSS",]
+        numberS = 0
 
-    def __init__(self, name, mtr ):
-        self.name = name
+        def __init__(self, name, vorname, mtr, course):
+            self.name = name
+            self.vorname = vorname
+            self.mtr = mtr
+            self.course = course
 
+        def get_details(self):
+            return self
 
-    def get_details(self):
-        print('test')
+        def enroll(self, course):
+            return
+
+        def get_no_of_students(self, numberS):
+            numberS += 1
+            return
+
+        def get_course_participants(self, course):
+            return
+
+        def __str__(self):
+            return
